6cb924bbbaf9c159e07fc561ae8eaa991b017abd
MohJamal <mohjamalalkhatib96@gmail.com>
2022-10-22 18:04:36 +0300

    Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "Ahmed",
+    last: "Zaid",
+  };
+
+  const namesList = [
+    {
+      first: "Ahmed2",
+      last: "Zaid2",
+    },
+    {
+      first: "Ahmed3",
+      last: "Zaid3",
+    },
+    {
+      first: "Ahmed4",
+      last: "Zaid4",
+    },
+  ];
+
   return (
     <div className="App">
-      <Greet name="Mohammed" />
+      <Greet name="Mohammed" messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={namesList} />
     </div>
   );
 }
