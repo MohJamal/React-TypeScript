560085fed7c01f8bfd8078e9d7fbd37948688a41
MohJamal <mohjamalalkhatib96@gmail.com>
2023-01-05 02:42:26 +0200

    16 - useRef Hook part2 => typing

--- src/components/refs/DomRef.tsx
+++ src/components/refs/DomRef.tsx
@@ -1,12 +1,13 @@
 import { useRef, useEffect } from "react";
 
 export const DomRef = () => {
-  const inputRef = useRef(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null!);
 
   useEffect(() => {
+    // inputRef.current?.focus();
     inputRef.current.focus();
   }, []);
-
   return (
     <div>
       <input type="text" ref={inputRef} />
