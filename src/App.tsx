00152b1d74b4cd8408d76f16c3b19568666a0cd0
MohJamal <mohjamalalkhatib96@gmail.com>
2023-01-07 00:31:07 +0200

    20 - Restricting Props part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { List } from "./components/generics/List";
+import { RandomNumber } from "./components/restriction/RandomNumber";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <List
-        items={["Batman", "Superman", "Wonder Woman"]}
-        onClick={(item) => console.log(item)}
-      />
-      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
-      <List
-        items={[
-          {
-            id: 1,
-            first: "Bruce",
-            last: "Wayne",
-          },
-          {
-            id: 2,
-            first: "Clark",
-            last: "Kent",
-          },
-          {
-            id: 3,
-            first: "Princess",
-            last: "Diana",
-          },
-        ]}
-        onClick={(item) => console.log(item)}
-      />
+      <RandomNumber value={10} isPositive isNegative isZero />
     </div>
   );
 }
