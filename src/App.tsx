1b360e4cd0b9fc17b25b7a7922f0c5a0fb7933cb
MohJamal <mohjamalalkhatib96@gmail.com>
2023-01-07 20:59:51 +0200

    24 - Polymorphic Components part2

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Text size="lg">Heading</Text>
-      <Text size="md">Paragraph</Text>
-      <Text size="sm" color="secondary">
+      {/* <Text size="lg" as="h1" htmlFor="Id">
+        Heading
+      </Text> */}
+      <Text size="lg" as="h1">
+        Heading
+      </Text>
+      <Text size="md" as="p">
+        Paragraph
+      </Text>
+      <Text size="sm" color="secondary" as="label" htmlFor="someId">
         Label
       </Text>
     </div>
