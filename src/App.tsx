dc29e70b1a60f8f262661dfc4944fb531de2ab05
MohJamal <mohjamalalkhatib96@gmail.com>
2023-01-07 16:32:52 +0200

    22 - Wrapping HTML Elements part4 => restrict CustomButton children type to just string (not string & React.ReactNode) by using Omit keyword

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <CustomButton variant="primary" onClick={() => console.log("Clicked")}>
+      {/* <CustomButton variant="primary" onClick={() => console.log("Clicked")}>
         <div>Button Label</div>
+      </CustomButton> */}
+
+      <CustomButton variant="primary" onClick={() => console.log("Clicked")}>
+        Button Label
       </CustomButton>
     </div>
   );
